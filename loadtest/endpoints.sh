ede73b1221a7e83569a7f6c1eccce5647bff2ae8
niezgodam <niezgodam@student.agh.edu.pl>
2026-03-29 12:07:20 +0000

    a,b scneario

--- loadtest/endpoints.sh
+++ loadtest/endpoints.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Endpoint URLs for load testing
 # Fill in after deploying with the URLs printed by each deploy script
-export LAMBDA_ZIP_URL=""        # e.g. https://<id>.lambda-url.us-east-1.on.aws
-export LAMBDA_CONTAINER_URL=""  # e.g. https://<id>.lambda-url.us-east-1.on.aws
-export FARGATE_URL=""           # e.g. http://<alb-dns>.us-east-1.elb.amazonaws.com
-export EC2_URL=""               # e.g. http://<public-ip>:8080
+export LAMBDA_ZIP_URL="https://gwanlgdx3rtjrlshamws6yarlq0elajh.lambda-url.us-east-1.on.aws"        # e.g. https://<id>.lambda-url.us-east-1.on.aws
+export LAMBDA_CONTAINER_URL="https://ps6lhlp6ijswf5pvc5jyddniqe0gxpki.lambda-url.us-east-1.on.aws"  # e.g. https://<id>.lambda-url.us-east-1.on.aws
+export FARGATE_URL="http://lsc-knn-alb-571991728.us-east-1.elb.amazonaws.com"           # e.g. http://<alb-dns>.us-east-1.elb.amazonaws.com
+export EC2_URL="http://3.234.221.114:8080"               # e.g. http://<public-ip>:8080
